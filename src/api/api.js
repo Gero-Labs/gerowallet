5e004ea23ad0f3fe9f20aab65d4df2db68b3269f
Dudi Edri <dudi@anyclip.com>
2024-05-09 10:04:01 +0300

    Commit

--- src/api/api.js
+++ src/api/api.js
@@ -10,31 +10,27 @@ export class Api {
             timeout: 60000,
             headers: {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
+                'Access-Control-Allow-Origin': '*',
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZGRyIjoic3Rha2UxdXltd2t6MnpsbjQwMDU3NGR1cjdkcnhmcHB2amFxbWNhNnc5OGswcW1scjc0bmcyZjVxY2wiLCJleHAiOjE3MjYxNjkwNjksInRpZXIiOjEsInByb2pJRCI6Ikdlcm8gV2FsbGV0In0.4sbiXIWJjqsNgC1oqRn3u8_Mj0qkSq3ODCnxQMGE7jE'
             },
         })
     }
 
-    async fetchAccountInfo(chain, network, address) {
+    async getAccountInfo(chain, network, address) {
+        console.log('getAcc')
         try {
             const rewardAddress = address.startsWith('addr') ? resolveRewardAddress(address) : address;
             const {data, status} = await this.axiosInstance.post('account_info', {
                 _stake_addresses: [rewardAddress],
             });
             if (status === 200)
-                return {
-                    poolId: data[0].delegated_pool,
-                    active: data[0].status === 'registered',
-                    balance: data[0].total_balance.toString(),
-                    rewards: data[0].rewards_available,
-                    withdrawals: data[0].withdrawals,
-                };
+                return data
             throw parseHttpError(data);
         } catch (error) {
             throw parseHttpError(error);
         }
     }
-    async getTip() {
+    async getTip(chain, network) {
         try {
             const {data, status} = await this.axiosInstance.get('tip');
             if (status === 200)
