f8be685c530a12dcf5223ab33a0bf1c0496d881e
ng-babyshark <eugeniuznagovan@gmail.com>
2024-06-03 17:07:18 +0300

    Revert "Removed vue.config.js"
    This reverts commit 69409ac888b4deb9e1c237eb0e4caf1aa9fe9c1a.

---
+++ vue.config.js
@@ -0,0 +1,44 @@
+const CopyPlugin = require('copy-webpack-plugin')
+const { defineConfig } = require('@vue/cli-service')
+const webpack = require('webpack');
+
+module.exports = defineConfig({
+  chainWebpack: config => {
+    config.plugin('html')
+        .tap(args => {
+          args[0].title = "GeroWallet | Your Master Key to DeFi";
+          return args;
+        })
+  },
+  transpileDependencies: [
+    'vuetify'
+  ],
+  configureWebpack: {
+    experiments: {
+      asyncWebAssembly: true
+    },
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          { from: 'src/assets', to: 'assets' },
+          { from: 'src/manifest.json', to: 'manifest.json' },
+          { from: 'src/background.js', to: 'background.js' },
+          { from: 'src/content.js', to: 'content.js' },
+          { from: 'src/inject.js', to: 'inject.js' }
+        ]
+      }),
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+        process: 'process/browser',
+      })
+    ],
+    resolve: {
+      extensions: ['.ts', '.js'],
+      fallback: {
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "buffer": require.resolve("buffer")
+      }
+    },
+  }
+})
