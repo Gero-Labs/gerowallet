e8ba451d63ab47c3bd2b6a2d1cb21fc3f64cdc4d
KyrSmaw <kirill86@gmail.com>
2024-11-29 13:09:47 -0500

    Added copy fucntionality to share code

--- src/modules/referal/components/ReferralBox.vue
+++ src/modules/referal/components/ReferralBox.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
     import { inject } from 'vue';
     import { RefInfo } from '../models';
+    import CopyButton from '@/shared/components/CopyButton.vue';
 
     const refInfo: RefInfo = inject('refInfo');
 </script>
@@ -15,7 +16,10 @@
         </v-row>
         <v-row>
             <v-col class="w-full gero-dark rounded mt-2" style="width: 100%; border: 1px solid #98A2B324">
-                <v-input :value="refInfo.refAddress" />
+                <v-input class="pa-1" style="color: #7ED8FF; font-size: 12px;" hide-details="true">{{ refInfo.refAddress }}</v-input>
+                <div class="copy-container">
+                    <CopyButton :value="refInfo.refAddress" small />
+                </div>
             </v-col>
         </v-row>
     </v-container>
@@ -31,8 +35,14 @@
         width: fit-content;
         flex: 0 1 auto;
         padding: 5px;
+        position: relative;
     }
     .v-chip{
         font-size: 12px;
+    }
+    .copy-container{
+        position: absolute;
+        right: 0.2rem;
+        top: 0.2rem;
     }
 </style>
