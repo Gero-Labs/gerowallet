13930df0a63b2dd327f67f84c04fc2b049251a73
KyrSmaw <kirill86@gmail.com>
2024-12-11 10:58:01 -0500

    init commit

--- src/modules/referal/models/index.ts
+++ src/modules/referal/models/index.ts
@@ -1,8 +1,13 @@
 export interface Referral{
+    walletAddress: string;
     name: string;
+    redeemDate: number; // represents timestamp
+    elibility: boolean;
+    reward: number;
 }
 export interface RefInfo{
     refAddress: string;
     referrals?: Referral[];
     currentView: 'refer' | 'redeem';
+
 }
