b97ce7c18d33b95a8904469b7558f84aefa7aecc
KyrSmaw <kirill86@gmail.com>
2024-12-13 10:32:05 -0500

    Setting up for the referrals table

--- src/modules/referal/models/index.ts
+++ src/modules/referal/models/index.ts
@@ -1,8 +1,14 @@
 export interface Referral{
     name: string;
+    address: string;
+    dateRedeemed: string;
+    eligible: boolean;
+    reward: number;
 }
 export interface RefInfo{
     refAddress: string;
     referrals?: Referral[];
     currentView: 'refer' | 'redeem';
+    totalRewards: number; // in $GERO
+    totalRewardsInADA: number // in ADA
 }
