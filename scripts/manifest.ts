e8d31c01d1722ddc8db9750fdaeea132eb39f0ba
dudiedri <dudi.edri@ox.security>
2026-07-02 19:01:26 +0300

    feat(cashback): allow Bring portal origin in CSP frame-src

--- scripts/manifest.ts
+++ scripts/manifest.ts
@@ -114,6 +114,7 @@ function buildCSP(dev: boolean): string {
     'https://www.kaiserex.com/',
     'https://kaiserex.com/',
     'https://forms.zohopublic.eu/',
+    'https://*.bringweb3.io/',
   ];
 
   const mediaSrc = [
